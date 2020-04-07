fff2d191ba570e32e61f9c7e15193b2bcbeb5c92
CHOcho-quan <574166505@qq.com>
2020-04-07 23:26:50 +0800

    2D-simple version kalman filter without control input

--- include/filtering/kalman_filter.hpp
+++ include/filtering/kalman_filter.hpp
@@ -8,8 +8,11 @@
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 
-class LinearDynamicSystem {
- public:
+#define DEBUG 1
+
+class LinearDynamicSystem
+{
+public:
     /**
      * Formulation of a linear dynamic system model, simple 2D version
      * Xt = A * Xt-1 + B + e1
@@ -21,13 +24,31 @@ class LinearDynamicSystem {
     Eigen::Matrix2d C; // Observation Matrix
     Eigen::Vector2d D;
     const int state_size = 2, obs_size = 2;
-	Eigen::Matrix2d Q, R; // Error Covarience Matrix
+    Eigen::Matrix2d Q, R; // Error Covarience Matrix
 
-    std::uniform_real_distribution<> n1, n2; // Normal Distribution
+    std::uniform_real_distribution<> n; // Normal Distribution
+
+    LinearDynamicSystem() {}
+    LinearDynamicSystem &operator=(LinearDynamicSystem &lds) {
+        A = lds.A;
+        B = lds.B;
+        C = lds.C;
+        D = lds.D;
+        Q = lds.Q;
+        R = lds.R;
+    }
+
+    LinearDynamicSystem(const LinearDynamicSystem &lds) {
+        A = lds.A;
+        B = lds.B;
+        C = lds.C;
+        D = lds.D;
+        Q = lds.Q;
+        R = lds.R;
+    }
 
     LinearDynamicSystem(double cov1, double cov2) {
-        n1 = std::uniform_real_distribution<>(0, cov1);
-        n2 = std::uniform_real_distribution<>(0, cov2);
+        n = std::uniform_real_distribution<>(0, 1);
         Q(0, 0) = cov1;
         Q(1, 1) = cov1;
         R(0, 0) = cov2;
@@ -36,13 +57,19 @@ class LinearDynamicSystem {
 
     Eigen::Vector2d getObservation(Eigen::Vector2d input, std::mt19937 gen) {
         Eigen::Vector2d result = C * input + D;
-        result(0) += n1(gen);
-        result(1) += n2(gen);
+        result(0) += n(gen) * Q(0, 0);
+        result(1) += n(gen) * Q(1, 1);
+
+        return result;
     }
 
     Eigen::Vector2d getNextState(Eigen::Vector2d last_state, std::mt19937 gen) {
         Eigen::Vector2d state = A * last_state + B;
-        state(0) += n1(gen);
-        state(1) += n2(gen);
+        state(0) += n(gen) * R(0, 0);
+        state(1) += n(gen) * R(1, 1);
+
+        return state;
     }
+
+    Eigen::Vector2d getReckonedNextState(Eigen::Vector2d last_state) { return A * last_state + B; }
 };
--- src/filtering/kalman_filter.cpp
+++ src/filtering/kalman_filter.cpp
@@ -18,7 +18,8 @@ class KalmanFilter {
     Eigen::Matrix2d last_covarience;
     LinearDynamicSystem lds;
 
-    KalmanFilter(LinearDynamicSystem s) : lds(s) {
+    KalmanFilter(LinearDynamicSystem s) {
+        lds = s;
         // Initial Eye matrix for covariences
         covarience(0, 0) = 1.0;
         covarience(1, 1) = 1.0;
@@ -27,20 +28,20 @@ class KalmanFilter {
     }
     void predict() {
         prediction = lds.A * filtered + lds.B;
+        // std::cout << "PREDICT: " << lds.A << std::endl;
         covarience = lds.A * last_covarience * lds.A.transpose() + lds.Q;
     }
     void update(Eigen::Vector2d obs) {
         kalman_gain = covarience * lds.C.transpose() * 
             (lds.C.transpose() * covarience * lds.C + lds.R).inverse();
-        filtered = prediction + kalman_gain * (obs - lds.C * prediction);
+        filtered = prediction + kalman_gain * (obs - lds.C * prediction - lds.D);
         last_covarience = covarience - kalman_gain * lds.C * covarience;
     }
 };
 
 int main()
 {
-    LinearDynamicSystem lds(1.0, 1.0);
-    KalmanFilter kf(lds);
+    LinearDynamicSystem lds(1, 1);
     lds.A << 1.0, 0.0, 
             0.0, 1.0;
     lds.B << 1.0, 1.0;
@@ -48,18 +49,39 @@ int main()
     lds.C << 1.0, 0.0,
             0.0, 1.0;
     lds.D << 1.0, 1.0;
+    KalmanFilter kf(lds);
+    std::cout << lds.A;
 
     // Initial State of position
-    Eigen::Vector2d pos(1.0, 1.0);
+    Eigen::Vector2d pos(1.0, 1.0), reckon(1.0, 1.0);
+    kf.filtered(0) = 1.0;
+    kf.filtered(1) = 1.0;
 
     // Randomized tools
     std::random_device rd;
     std::mt19937 gen(rd());
 
+    cv::namedWindow("Kalman Filter");
+    cv::Mat background = cv::Mat(500, 500, CV_8UC3, cv::Scalar(255, 255, 255));
     double dT = 0.1, T = 50;
-    for (dT;dT < T;dT += 0.1) {
+    for (double t = 0.0;t < T;t += dT) {
+        pos = lds.getNextState(pos, gen);
+        reckon = lds.getReckonedNextState(reckon);
         Eigen::Vector2d obs = lds.getObservation(pos, gen);
+
+        // Visualizing
+        cv::circle(background, cv::Point(int(reckon(0)), int(reckon(1))), 5, cv::Scalar(0, 0, 0), -1);
+        cv::circle(background, cv::Point(int(pos(0)), int(pos(1))), 5, cv::Scalar(0, 255, 0), -1);
+        cv::circle(background, cv::Point(int(kf.filtered(0)), int(kf.filtered(1))), 5, cv::Scalar(255, 0, 0), -1);
+        cv::circle(background, cv::Point(int(obs(0)), int(obs(1))), 5, cv::Scalar(0, 0, 255), -1);
+
+        if (DEBUG) std::cout << "KF: " << kf.filtered(0) << ' ' << kf.filtered(1) << std::endl;
+        if (DEBUG) std::cout << "POS: " << pos(0) << ' ' << pos(1) << std::endl;
+        if (DEBUG) std::cout << "OBS: " << obs(0) << ' ' << obs(1) << std::endl;
+
         kf.predict();
         kf.update(obs);
     }
+    cv::imshow("Kalman Filter", background);
+    cv::waitKey(0);
 }
